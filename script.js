bb97fd22f9206ef9277914817c0b9e7a2f7b79d3
atletlgomez <107003206+atletlgomez@users.noreply.github.com>
2024-03-19 23:13:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//localStorage.setItem("Hello", "hello item");
+///const testItem = localStorage.getItem("Hello");
+
+function save() {
+  let textValue = document.getElementById("answer").value;
+  console.log(textValue);
+  localStorage.setItem("textfield", textValue);
+}
+
+function retrieve() {
+  const storeditem = localStorage.getItem("textfield");
+  document.getElementById("box2").innerHTML(`${storeditem}`);
+}
